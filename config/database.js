10473ca668ee097e5f43ea3bbed91295a784c930
Mayur Dattatraya Kamthe <128322330+Mayurkamthe@users.noreply.github.com>
2026-04-07 06:51:02 +0530

    Db fix

--- config/database.js
+++ config/database.js
@@ -7,8 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     pool: {
       max: 10,
       min: 0,
